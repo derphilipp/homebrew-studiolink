48a2ccdad53367f1f81c2617c31baf96d28ee5b5
Philipp <mail@philipp-weissmann.de>
2021-04-03 00:50:30 +0200

    Initial version for studiolink-plugin

---
+++ Casks/studiolink-plugin.rb
@@ -0,0 +1,11 @@
+cask "studiolink-plugin" do
+  version "21.03.2"
+  sha256 "54c8e41112a27413afc4b37449474583c61319b7a43d1bf4380b5d4256db4a7c"
+
+  url "https://download.studio.link/releases/v#{version}-stable/macos_x86_64/signed/studio-link-plugin.zip", verified: "download.studio.link"
+  name "Studiolink"
+  desc "High quality Audio-over-IP Connections"
+  homepage "https://studio-link.de/"
+
+  audio_unit_plugin "StudioLink.component"
+end
